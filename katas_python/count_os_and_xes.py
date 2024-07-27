20c5cacc4643afb078f445f461d220d769de36c7
Veselin Martionv <veselin.martinov@gmail.com>
2024-07-27 22:43:32 +0200

    How_many_are_smaller_than_me_solution

--- katas_python/count_os_and_xes.py
+++ katas_python/count_os_and_xes.py
@@ -1,7 +1,9 @@
 # we receive a line containing various symbols
 # count whether the number of 'o's equals the number of 'x's
 def xo(s):
+    # convert the string to lowercase
     s = s.lower()
+    # take advantage of the built-in count function and compare the number of 'o's and 'x's
     return s.count('o') == s.count('x')
 
 
