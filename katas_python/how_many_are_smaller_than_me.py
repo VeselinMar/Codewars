d88860dc50bb580ca2af77cf137c61ab31a4c0cb
Veselin Martionv <veselin.martinov@gmail.com>
2024-07-28 20:48:10 +0200

    added comments to how_many_are_smaller_than_me.py

--- katas_python/how_many_are_smaller_than_me.py
+++ katas_python/how_many_are_smaller_than_me.py
@@ -2,10 +2,10 @@ from collections import deque
 
 
 def smaller(arr):
-    # we save our result in a list
+    # save our result in a list
     result = []
-    # we define a work list(deque) and fill it with our numbers
-    # we use a deque because we want to start from the left
+    # define a work list(deque) and fill it with our numbers
+    # use a deque because we want to start from the left
     work_list = deque(arr)
     for _ in range(len(arr)):
         number = work_list.popleft()
@@ -14,8 +14,7 @@ def smaller(arr):
             if n < number:
                 count += 1
         result.append(count)
-        # we added the popped number to the end of the list because we need to count
-        # whether the number is smaller than the following numbers
+        # add the popped number to the end of the list still needs to be used in further counts
         work_list.append(number)
 
     return result
