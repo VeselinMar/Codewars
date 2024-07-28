12775ae416e61c23e7d96a5c59fd82301e0e5cd3
Veselin Martionv <veselin.martinov@gmail.com>
2024-07-28 21:05:47 +0200

    provided a new more pythonic solution and added comments to narcissistic_number.py

--- katas_python/narcissistic_number.py
+++ katas_python/narcissistic_number.py
@@ -1,11 +1,13 @@
+# function to check whether a number is Armstrong(narcissistic) number
 def narcissistic(value):
+    # convert number to string
     num_str = str(value)
-    sum_num = 0
-    for n in num_str:
-        sum_num += int(n) ** len(num_str)
-    if sum_num == int(value):
-        return True
-    return False
+    # get length of number
+    num_len = len(num_str)
+    # use a generator to calculate each number lifted to the power of num_len then sum the numbers
+    sum_num = sum(int(n) ** num_len for n in num_str)
+    # Return True or False depending on result
+    return sum_num == value
 
 
 line = input()
