5f6cb26d70f68fb6a2199dea156cbb58d0440b88
Veselin Martionv <veselin.martinov@gmail.com>
2024-07-28 20:59:02 +0200

    provided a new more pythonic solution and added comments to jadenize.py

--- katas_python/jadenize.py
+++ katas_python/jadenize.py
@@ -1,12 +1,9 @@
 def to_jaden_case(string):
-    new_string = ''
-    string_to_list = string.split(' ')
-    if string:  # execute only if string is valid
-        for item in range(len(string_to_list)):  # capitalize each first symbol in string
-            new_string += string_to_list[item][0].upper() + string_to_list[item][1::].lower()
-            if item != len(string_to_list) - 1:  # each item is followed by a space char except the last one
-                new_string += ' '
-        return new_string
+    # execute only if string is valid
+    if string:
+        new_string = ''
+        # capitalize each first symbol in string using the built-in function capitalize()
+        return ' '.join([word.capitalize() for word in string.split(' ')])
     return ''
 
 
